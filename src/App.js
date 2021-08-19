9f3dadc4cf65472631b3d793b932e0663b706639
jennifertimbol <jennifertmbl@gmail.com>
2021-08-19 14:01:10 -0700

    Components Pt 2

--- src/App.js
+++ src/App.js
@@ -2,20 +2,40 @@ import React, { Component } from 'react';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import './App.css';
 import Directory from './components/DirectoryComponent';
+import { CAMPSITES } from '.shared/campsites';
+
+// class App extends Component {
+//   render() {
+//     return (
+//       <div className= "App">
+//         <Navbar dark color="primary">
+//           <div className="container">
+//             <NavbarBrand href="/">NuCamp</NavbarBrand>
+//           </div>
+//         </Navbar>
+//         <Directory/>
+//       </div>
+//     );
+//   }
+// }
 
 class App extends Component {
-  render() {
-    return (
-      <div className= "App">
+  constructor(props) {
+    super(props);
+    this.state = {
+      campsites: CAMPSITES
+    };
+      return (
+        <div className= "App">
         <Navbar dark color="primary">
           <div className="container">
             <NavbarBrand href="/">NuCamp</NavbarBrand>
           </div>
         </Navbar>
-        <Directory/>
+        <Directory campsites={this.state.campsites} />
       </div>
-    );
+      );
+    }
   }
-}
 
 export default App;
